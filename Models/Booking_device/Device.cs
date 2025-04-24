b8b53b454ba8162513b4540610d9bd815167771c
4honestobs <huynhtien180103@gmail.com>
2025-04-25 01:33:13 +0700

    modified violation html

---
+++ Models/Booking_device/Device.cs
@@ -0,0 +1,10 @@
+﻿namespace QLThanhvien_Web.Models
+{
+    public class Device
+    {
+        public string? device_id { get; set; }
+        public string? device_name { get; set; }
+        public string? device_type { get; set; }
+        public string? status { get; set; } = "hoạt động";
+    }
+}
